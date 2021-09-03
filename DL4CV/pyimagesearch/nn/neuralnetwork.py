6632c22a0082889574c30cf632a0f280a5e9c536
Asilbek <asilbekyuldashev07@gmail.com>
2021-09-03 20:26:42 +0900

    Updates neuralnetwork.py and adds nn_XOR.py

--- DL4CV/pyimagesearch/nn/neuralnetwork.py
+++ DL4CV/pyimagesearch/nn/neuralnetwork.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 class NeuralNetwork:
-    def __init__(self, layers, alpha=0.1):
+    """    def __init__(self, layers, alpha=0.1):
         self.W = []
         self.layers = layers
         self.alpha = alpha
@@ -12,7 +12,19 @@ class NeuralNetwork:
             self.W.append(w / np.sqrt(layers[i]))
             # the last two layers are a special case where the input
             # connections need a bias term but the output does not
-            w = np.random.randn(layers[-2] + 1, layers[-1])
+            w = np.random.randn(layers[-2] + 1, layers[-1])"""
+    def __init__(self, layers, alpha=0.1):
+        self.W = []
+        self.layers = layers
+        self.alpha = alpha
+
+        for i in np.arange(0, len(layers) - 2):
+            w = np.random.randn(layers[i] + 1, layers[i + 1] + 1)
+            self.W.append(w / np.sqrt(layers[i]))
+        # the last two layers are a special case where the input
+        # connections need a bias term but the output does not
+        w = np.random.randn(layers[-2] + 1, layers[-1])
+        self.W.append(w / np.sqrt(layers[-2]))
 
     def __repr__(self):
         # this represents network architecture and returns string
@@ -47,7 +59,7 @@ class NeuralNetwork:
     def fit_partial(self, x, y):
         """Bu Algoritm har bir layer ning activatsika funksiyasi va weight orasidagi dot
         productni oladi va uni nonlinear bolgan bolgan aktivatsiya funkisyasi sigmoid
-        ga otkazadi va keyingi layerga o'tkazadi"""
+        ga otkazadi va keyingi layerga o'tadi"""
         A = [np.atleast_2d(x)]
         for layer in np.arange(0, len(self.W)):
 
@@ -56,4 +68,49 @@ class NeuralNetwork:
             out = self.sigmoid(net)
 
             A.append(out)
+        error = A[-1] - y
+
+        D = [error * self.sigmoid_deriv(A[-1])]
+
+        for layer in np.arange(len(A) - 2, 0, -1):
+            delta = D[-1].dot(self.W[layer].T)
+            delta = delta * self.sigmoid_deriv(A[layer])
+            D.append(delta)
+
+        D = D[::-1]
+        for layer in np.arange(0, len(self.W)):
+            self.W[layer] += -self.alpha * A[layer].T.dot(D[layer])
+
+    def predict(self, X, addBias=True):
+        """p = np.atleast_2d(X)
+
+        if addBias:
+            p = np.c_[p, np.ones((p.shape[0]))]
+
+        for layer in np.arange(0, len(self.W)):
+            p = self.sigmoid(np.dot(p, self.W[layer]))
+
+        return p"""
+        p = np.atleast_2d(X)
+
+        if addBias:
+            p = np.c_[p, np.ones((p.shape[0]))]
+
+        for layer in np.arange(0, len(self.W)):
+            p = self.sigmoid(np.dot(p, self.W[layer]))
+
+        return p
+
+    def calculate_loss(self, X, target):
+        """targets = np.atleast_2d(target)
+        predictions = self.predict(X, addBias=False)
+        loss = 0.5 * np.sum((predictions - targets) ** 2)
+
+        return loss"""
+        targets = np.atleast_2d(target)
+        predictions = self.predict(X, addBias=False)
+        loss = 0.5 * np.sum((predictions - targets) ** 2)
+
+        return loss
+
 
