b4a1df7984c551d35cdc515658456276909ae456
asil07 <asilbekyuldashev07@gmail.com>
2021-10-16 11:24:48 +0900

    updated

--- PB__DL4_CV/ch11_GoogLeNet/deepergooglenet/pyimagesearch/nn/conv/deepergooglenet.py
+++ PB__DL4_CV/ch11_GoogLeNet/deepergooglenet/pyimagesearch/nn/conv/deepergooglenet.py
@@ -42,14 +42,13 @@ class DeeperGoogLeNet:
         # third branch
         third = DeeperGoogLeNet.conv_module(x, num5x5Reduce, kX=1, kY=1, stride=(1, 1),
                                             chanDim=chanDim, reg=reg, name=stage + "_third1")
-        third = DeeperGoogLeNet.conv_module(third, num5x5, kX=1, kY=1, stride=(5, 5),
+        third = DeeperGoogLeNet.conv_module(third, num5x5, kX=5, kY=5, stride=(1, 1),
                                             chanDim=chanDim, reg=reg, name=stage + "_third2")
         # Fourth branch
         fourth = MaxPooling2D(pool_size=(3, 3), strides=(1, 1), padding="same", name=stage + "_pool")(x)
 
         fourth = DeeperGoogLeNet.conv_module(fourth, num1x1Proj, 1, 1, stride=(1, 1), chanDim=chanDim,
                                              reg=reg, name=stage + "_fourth")
-
 
         x = concatenate([first, second, third, fourth], axis=chanDim, name=stage + "_mixed")
 
@@ -67,7 +66,7 @@ class DeeperGoogLeNet:
 
         inputs = Input(shape=inputShape)
 
-        x = DeeperGoogLeNet.conv_module(inputs, K=64, kX=5, kY=5, stride=(5, 5), chanDim=chanDim,
+        x = DeeperGoogLeNet.conv_module(inputs, K=64, kX=5, kY=5, stride=(1, 1), chanDim=chanDim,
                                         reg=reg, name="block-1")
         x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding="same", name="pool-1")(x)
         x = DeeperGoogLeNet.conv_module(x, 64, 1, 1, (1, 1), chanDim, reg=reg, name="block-2")
@@ -82,6 +81,34 @@ class DeeperGoogLeNet:
                                              num5x5Reduce=32, num5x5=96, num1x1Proj=64,
                                              chanDim=chanDim, stage="3b", reg=reg)
         x = MaxPooling2D((3, 3), strides=(2, 2), padding="same", name="pool-3")(x)
+
+        # applying five Inception modules followed by Pool
+        x = DeeperGoogLeNet.inception_module(x=x, num1x1=192, num3x3Reduce=96, num3x3=208, num5x5Reduce=16,
+                                             num5x5=48, num1x1Proj=64, chanDim=chanDim, stage="4a", reg=reg)
+        x = DeeperGoogLeNet.inception_module(x=x, num1x1=160, num3x3Reduce=112, num3x3=224, num5x5Reduce=24,
+                                             num5x5=64, num1x1Proj=64, chanDim=chanDim, stage='4b', reg=reg)
+        x = DeeperGoogLeNet.inception_module(x=x, num1x1=128, num3x3Reduce=128, num3x3=256, num5x5Reduce=24,
+                                             num5x5=64, num1x1Proj=64, chanDim=chanDim, stage="4c", reg=reg)
+        x = DeeperGoogLeNet.inception_module(x, 112, 144, 288, 32, 64, 64, chanDim, stage="4d", reg=reg)
+        x = DeeperGoogLeNet.inception_module(x=x, num1x1=256, num3x3Reduce=160, num3x3=320, num5x5Reduce=32,
+                                             num5x5=128, num1x1Proj=128, chanDim=chanDim, stage="4e", reg=reg)
+        x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding='same', name="pool-4")(x)
+
+        # applying a POOl layer average followed  by Dropout
+        x = AveragePooling2D(pool_size=(4, 4), name="pool-5")(x)
+        x = Dropout(0.4, name="Dropout")(x)
+
+        # softmax classifier
+        x = Flatten(name="flatten")(x)
+        x = Dense(classes, kernel_regularizer=l2(reg), name="labels")(x)
+        x = Activation("softmax", name="softmax")(x)
+
+        model = Model(inputs, x, name="googlenet")
+
+        return model
+
+
+
 
 
 
