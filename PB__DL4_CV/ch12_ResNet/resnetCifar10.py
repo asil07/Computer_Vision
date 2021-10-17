60f4e7c7dfaaffad01ece6255e08426d4cb57f61
asil07 <asilbekyuldashev07@gmail.com>
2021-10-17 17:42:24 +0900

    ResNet on cifar10 dataset with residual module

---
+++ PB__DL4_CV/ch12_ResNet/resnetCifar10.py
@@ -0,0 +1,71 @@
+import matplotlib
+matplotlib.use("Agg")
+
+from sklearn.preprocessing import LabelBinarizer
+from pyimagesearch.nn.conv.resnet import ResNet
+from pyimagesearch.callbacks.epochcheckpoint import EpochCheckpoint
+from pyimagesearch.callbacks.trainingmonitor import TrainingMonitor
+from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import SGD
+from keras.datasets import cifar10
+from keras.models import load_model
+import keras.backend as K
+import numpy as np
+import argparse
+import sys
+
+sys.setrecursionlimit(5000)
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-c", "--checkpoints", required=True)
+ap.add_argument("-m", "--model", type=str)
+ap.add_argument("-s", "--start-epoch", type=int, default=0)
+args = vars(ap.parse_args())
+
+print("INFO: laoding dataset cifar10...")
+((trainX, trainY), (testX, testY)) = cifar10.load_data()
+trainX = trainX.astype("float")
+testX = testX.astype("float")
+
+mean = np.mean(trainX, axis=0)
+trainX -= mean
+testX -= mean
+
+lb = LabelBinarizer()
+trainY = lb.fit_transform(trainY)
+testY = lb.transform(testY)
+
+
+aug = ImageDataGenerator(height_shift_range=0.1, width_shift_range=0.1, horizontal_flip=True,
+                         fill_mode="nearest")
+
+if args['model'] is None:
+    print("INFO: compiling model....")
+    opt = SGD(learning_rate=1e-1)
+    model = ResNet.build(width=32, height=32, depth=3, classes=10,
+                         stages=(9, 9, 9), filters=(64, 64, 128, 256), reg=0.0005)
+    model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
+
+else:
+    print(f"INFO loading {args['model']}...")
+    model = load_model(args["model"])
+
+    print(f"INFO old-learning rate: {K.get_value(model.optimizer.lr)}")
+    K.set_value(model.optimizer.lr, 1e-5)
+    print(f"INFo new learning rate {K.get_value(model.optimizer.lr)}")
+
+callbacks = [
+    EpochCheckpoint(args["checkpoints"], every=1, startAt=args["start_epoch"]),
+    TrainingMonitor("output/resnet56_cifar10.png", jsonPath="output/resnet56cifar.json",
+                    startAt=args['start_epoch'])
+
+]
+
+# ===================================
+print("Info: training network...")
+model.fit(aug.flow(trainX, trainY, batch_size=128), validation_data=(testX, testY),
+          steps_per_epoch=len(trainX) // 128, epochs=100, callbacks=callbacks, verbose=1)
+
+
+
+
