bbefa7c461997f1d3580d2b75d831d71875753ef
asilbekyuldashev <asilbekyuldashev07@gmail.com>
2021-11-17 15:45:14 +0900

    to be continued...

--- PB__DL4_CV/ch16_GANs/dcgan_mnist.py
+++ PB__DL4_CV/ch16_GANs/dcgan_mnist.py
@@ -18,7 +18,38 @@ args = vars(ap.parse_args())
 
 NUM_EPOCHS = args["epochs"]
 BATCH_SIZE = args["batch_size"]
+
 print("INFO: loading MNIST dataset...")
+((trainX, _), (testX, _)) = mnist.load_data()
+trainImages = np.concatenate([trainX, testX])
+
+trainImages = np.expand_dims(trainImages, axis=-1)
+trainImages = (trainImages.astype("float") - 127.5) - 127.5
+
+print("INFO: building generator...")
+gen = DCGAN.build_generator(7, 64, channels=1)
+
+print("INFO: building discriminator...")
+disc = DCGAN.build_discriminator(28, 28, 1)
+discOpt = Adam(lr=0.0002, beta_1=0.5, decay=0.0002 / NUM_EPOCHS)
+disc.compile(loss="binary_crossentropy", optimizer=discOpt)
+
+print("INfo: building GAN ... ")
+disc.trainable = False
+ganInput = Input(shape=(100,))
+ganOutput = disc(gen(ganInput))
+gan = Model(ganInput, ganOutput)
+
+ganOpt = Adam(lr=0.0002, beta_1=0.5, decay=0.0002 / NUM_EPOCHS)
+gan.compile(loss="binary_crossentropy", optimizer=discOpt)
+
+print("INfo: starting training...")
+benchmarkNoise = np.random.uniform(-1, 1, size=(256, 100))
+
+for epoch in range(NUM_EPOCHS):
+    print(f"INFO: starting epoch {epoch + 1} of {NUM_EPOCHS}...")
+    batchesPerEpoch = int(trainImages.shape[0] / BATCH_SIZE)
+
 
 
 
