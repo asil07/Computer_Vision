87b637f14bbb5cd586d1deffde04fdf2b4aac1ad
asilbekyuldashev <asilbekyuldashev07@gmail.com>
2021-11-29 17:00:26 +0900

    training dataset for the SRCNN

---
+++ PB__DL4_CV/ch17_Image_resolution/build_dataset.py
@@ -0,0 +1,35 @@
+from pyimagesearch.io.hdf5datasetwriter import HDF5DatasetWriter
+from conf import sr_config as config
+from imutils import paths
+from scipy import misc
+import shutil
+import random
+import cv2
+import os
+
+# creates output directries if not exist
+for p in [config.IMAGES, config.LABELS]:
+    if not os.path.exists(p):
+        os.makedirs(p)
+
+print("INFO: creating temporary images ... ")
+imagePaths = list(paths.list_images(config.INPUT_IMAGES))
+random.shuffle(imagePaths)
+total = 0
+
+for imagePath in imagePaths:
+    image = cv2.imread(imagePath)
+
+    (h, w) = image.shape[:2]
+    w -= int(w % config.SCALE)
+    h -= int(h % config.SCALE)
+    image = image[0:h, 0:w]
+
+    scaled = misc.imresize(image, 1.0 / config.SCALE, interp="bicubic")
+    scaled = misc.imresize(scaled, config.SCALE/1.0, interp="bicubic")
+
+
+
+
+
+
